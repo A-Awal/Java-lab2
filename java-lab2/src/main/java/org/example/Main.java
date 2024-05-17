90d4b887d5a839a242bb95ba1c5a80ddd8f757ad
A-Awal <abdulai.awal@amalitech.org>
2024-05-17 09:43:42 +0000

    Completed

--- java-lab2/src/main/java/org/example/Main.java
+++ java-lab2/src/main/java/org/example/Main.java
@@ -8,11 +8,22 @@ import Part_3.P3_Main;
 public class Main {
     public static void main(String[] args) {
 
-//        P1_Main.RunLinkedListExercise();
-//        P1_Main.RunStudentListManger();
-//        P2_Main.RunCalculator();
-//        P2_Main.RunTaskScheduler();
-//        P3_Main.RunSearchAgos();
+        // Test linked list exercise
+       P1_Main.RunLinkedListExercise();
+
+       // Tes studunet listmanager
+       P1_Main.RunStudentListManger();
+
+       //Test calculator
+       P2_Main.RunCalculator();
+
+       //test task schedular
+       P2_Main.RunTaskScheduler();
+
+       //Test search algos
+       P3_Main.RunSearchAgos();
+
+       //Test sort algos
         P3_Main.runSortAlgos();
 
     }
